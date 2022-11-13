b8604c4f30fdbf3b618db88edd694cadaad779af
shati <shatilova_ira@mail.ru>
2022-11-13 12:07:51 +0600

    1 Task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Homework 3");
+        System.out.println(" ");
+        //Задача 1
+        System.out.println("Задача 1");
+        int apple = 5;
+        System.out.println("Значение переменной apple с типом int равно " + apple);
+        byte pear = 8;
+        System.out.println("Значение переменной pear с типом byte равно " + pear);
+        short peach = 3;
+        System.out.println("Значение переменной peach с типом short равно " + peach);
+        long plum = 15;
+        System.out.println("Значение переменной plum с типом long равно " + plum);
+        float plumWeight = 35.5f;
+        System.out.println("Значение переменной plumWeight с типом float равно " + plumWeight);
+        double appleWeight = 5.5;
+        System.out.println("Значение переменной appleWeight с типом double равно " + appleWeight);
     }
 }
